f1c3510fdf793330b26b8f1616b1dfb5777d0554
Michael Hammer <mhammer708@gmail.com>
2020-07-27 17:43:25 -0500

    hmm

---
+++ build/precache-manifest.94f469d7eab44eafffdf24f58b995432.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74be43b9fdcf0b24191391c8b54356a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd10d28fa01fa052eb4d",
+    "url": "/static/css/main.3de2283c.chunk.css"
+  },
+  {
+    "revision": "0f049a0c9dc39528db80",
+    "url": "/static/js/2.ef257060.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ef257060.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd10d28fa01fa052eb4d",
+    "url": "/static/js/main.613bda22.chunk.js"
+  },
+  {
+    "revision": "9e3404c951205a40d4fa",
+    "url": "/static/js/runtime-main.438c7e9c.js"
+  }
+]);
